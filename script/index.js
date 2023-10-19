5d5b0e5c3737871beaf1a0c6c4386a6a88d59d04
RamadanDani <ramadan.dani97@gmail.com>
2023-10-19 23:13:06 +0700

    Update javascript renderTemplate

--- script/index.js
+++ script/index.js
@@ -2,16 +2,16 @@
 const body = document.querySelector('.body');
 
 // Popup Edit Name
-const showPopupEditBtn = document.querySelector('.show_popup_edit-btn');
+const showPopupEditBtn = document.querySelector('.navigation__id-showPopupEditBtn');
 const submitEditNameBtn = document.querySelector('.popup__ename-button-submit');
-const closePopupEditBtn = document.querySelector('.close_popup_edit-btn');
+const closePopupEditBtn = document.querySelector('.popup__close-editBtn');
 const open = document.querySelector('.popup__ename');
 
 
 // Popup Add Picture
-const showPopupAddBtn = document.querySelector('.show_popup_add-btn');
+const showPopupAddBtn = document.querySelector('.navigation__id-showPopupAddPlaceBtn');
 const apicture = document.querySelector('.popup__apicture');
-const closeAddPictureBtn = document.querySelector('.popup_apicture_close-btn');
+const closeAddPictureBtn = document.querySelector('.popup__apicture-close-btn');
 const submitAddPhoto = document.querySelector('.popup__apicture-button-submit');
 
 
@@ -20,29 +20,27 @@ const userName = document.querySelector('.navigation__id-name');
 const userJob = document.querySelector('.navigation__id-job');
 const formName = document.querySelector('#nama');
 const formJob = document.querySelector('#job');
+const nilaiJudul = document.querySelector('#Judul');
+const nilaiUrl = document.querySelector('#url');
 
 
 
-
-
-
-const like = document.getElementsByClassName('content__images-id-button');
-
-
+// Template
 const contentCollection = document.querySelector('.content');
+const gallery = document.querySelector('.content--gallery');
 const contents = document.getElementsByClassName('content__images-list');
 const cName = document.getElementsByClassName('content__images-id-name');
 
-const deletePicture = document.getElementsByClassName('content__images-delete');
+const deletePicture = document.getElementsByClassName('content__images-delete-btn');
 const popupShowPicture = document.querySelector('.popup__vpicture');
 const pic = document.querySelector('.popup__vpicture-pic');
 const place = document.querySelector('.popup__vpicture-place');
-const closeShowPictureBtn = document.querySelector('.popup_vpicture_close-btn');
+const closeShowPictureBtn = document.querySelector('.popup__vpicture-close-btn');
 
 
 
-const gallery = document.querySelector('.content--gallery');
 
+// Data Template
 const initialCards = [
     {
       name: "Lembah Yosemite",
@@ -68,30 +66,32 @@ const initialCards = [
       name: "Lago di Braies",
       link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg"
     }
-  ];
+];
 
 
 
 
-
+// Menampilkan Form tambah gambar
 showPopupAddBtn.addEventListener('click', function(){
-    apicture.style.opacity = '1';
-    apicture.style.top = '50%';
+    apicture.classList.toggle('popup__none');
     body.style.opacity = '.5';
 });
 
 
 
+// Menutup Form tambah gambar
 closeAddPictureBtn.addEventListener('click', function (){
-    apicture.style.opacity = '.5';
-    apicture.style.top = '-50%';
+    apicture.classList.toggle('popup__none');
     body.style.opacity = '1';
 })
 
 
+
+
+// Manipulasi EDIT FORM START
 //Membuka Form Edit
 showPopupEditBtn.addEventListener('click', function () {
-    open.classList.toggle('popup__ename-open');
+    open.classList.toggle('popup__none');
     formName.value = userName.textContent;
     formJob.value = userJob.textContent;
     body.style.opacity = '.5';
@@ -100,7 +100,7 @@ showPopupEditBtn.addEventListener('click', function () {
 
 // Menutup Form Edit
 closePopupEditBtn.addEventListener('click', function () {
-    open.classList.remove('popup__ename-open')
+    open.classList.toggle('popup__none')
     body.style.opacity = '1';
 });
 
@@ -112,130 +112,90 @@ submitEditNameBtn.addEventListener('click', function () {
 
     userName.textContent = editName;
     userJob.textContent = editJob;
-    open.classList.remove('popup__ename-open')
+    open.classList.toggle('popup__none')
     body.style.opacity = '1';
 });
+
+// Manipulasi EDIT FORM FINISH
 
 
 //Menutup Popup Gambar
 closeShowPictureBtn.addEventListener('click', function(){
-    popupShowPicture.style.display ='none';
+    popupShowPicture.classList.toggle('popup__none');
     body.style.opacity = '1'
 });
 
 
-// Fungsi Menambah Gambar
-// function addPhoto(titleValue, urlValue){
 
-//     const photoContainer = document.createElement('div');
-//     photoContainer.classList.add('content__images');
-
-//     const imgPhoto = document.createElement('img');
-//     imgPhoto.classList.add('content__images-list');
-//     imgPhoto.src = urlValue;
-
-//     const deleteElement = document.createElement('button');
-//     deleteElement.classList.add('content__images-delete');
-//     deleteElement.setAttribute("type", "button");
-
-//     const imagesId = document.createElement('div');
-//     imagesId.classList.add('content__images-id');
- 
-//     const imagesIdName = document.createElement('p');
-//     imagesIdName.classList.add('content__images-id-name');
-//     imagesIdName.textContent = titleValue;
- 
-//     const buttonLove = document.createElement('button');
-//     buttonLove.classList.add('content__images-id-button');
- 
-
-//     photoContainer.append(imgPhoto, deleteElement, imagesId);
-//     imagesId.append(imagesIdName, buttonLove);
-//     contentCollection.prepend(photoContainer);
-
-//     }
-
-
-
-    // Event Menambah Gambar
-submitAddPhoto.addEventListener('click', function(e){
-    e.preventDefault();
-
-    const nilaiJudul = document.querySelector('#Judul');
-    const nilaiUrl = document.querySelector('#url');
-
-    const newCard = {
-        name: nilaiJudul.value,
-        link: nilaiUrl.value
-    };
-
-    initialCards.unshift(newCard);
-    
-    apicture.style.opacity = '0';
-    apicture.style.top = '-50%'
-    body.style.opacity = '1'
-
-    renderTemplate(initialCards);
-});
-
+  
+// Fungsi Render Gambar dari data Template
 function renderTemplate(data) {
     contentCollection.innerHTML ='';
     
     for (let i = 0; i < data.length; i++){
+        const tempat = data[i].name;
+        const url = data[i].link;
+
         const cardContent = gallery.content;
         const cloneContent = cardContent.cloneNode(true);
 
         const cardPlace = cloneContent.querySelector('.content__images-id-name');
         const cardImg = cloneContent.querySelector('.content__images-list');
 
-        cardPlace.textContent = data[i].name;
-        cardImg.src = data[i].link;
+        cardPlace.textContent = tempat;
+        cardImg.src = url;
+        
+
+        // Event Popup Gambar
+        cardImg.addEventListener('click', function(){
+            popupShowPicture.classList.toggle('popup__none');
+            pic.src = url;
+            place.innerHTML = tempat;
+            body.style.opacity ='.5';
+            
+        });
+
+        const cardTrash = cloneContent.querySelector('.content__images-delete-btn');
+
+        cardTrash.addEventListener('click', function(){
+            initialCards.splice(i,1);
+            renderTemplate(initialCards);
+        });
+
+        const cardLove = cloneContent.querySelector('.content__images-love-button');
+
+        cardLove.addEventListener('click', function(){
+            cardLove.classList.toggle('content__images-loved')
+        });
+
 
         contentCollection.appendChild(cloneContent)
     }
 }
 
 
+  // Event Menambah Gambar
+  submitAddPhoto.addEventListener('click', function(){
+    namaTempat = nilaiJudul.value;
+    urlGambar = nilaiUrl.value;
 
-// Popup Gambar
+    initialCards.unshift({
+        name: namaTempat,
+        link: urlGambar
+    })
+    
+    apicture.classList.toggle('popup__none');
+    body.style.opacity = '1'
 
-function ShowPictureOnBigScreen (data){
-    for (let i = 1; i < data.length; i++) {    
-        data[i].addEventListener('click', function(){
-            pic.src = data[i].src;
-            place.innerHTML = cName[i].textContent;
-            popupShowPicture.style.display = 'block';
-            body.style.opacity ='.5'
-        })
-    };
-};
+    nilaiJudul.value = '';
+    nilaiUrl.value = '';
+
+
+
+    renderTemplate(initialCards);
+});
+
+
+
 
 renderTemplate(initialCards);
-ShowPictureOnBigScreen(contents);
-
-for (let i = 0; i < contents.length; i++) {
-    
-    contents[i].addEventListener('click', function(){
-        pic.src = contents[i].src;
-        place.innerHTML = cName[i].textContent;
-        popupShowPicture.style.display = 'block';
-        body.style.opacity ='.5'
-    })
-};
-
-
-// Hapus Gambar
-for (let i = 0; i < deletePicture.length; i++ ) {
-    deletePicture[i].addEventListener('click', function(){
-        deletePicture[i].parentElement.style.display = 'none';
-    });
-};
-
-
-// Menyukai Gambar
-for (let i = 0; i < like.length; i++) {
-    like[i].addEventListener('click', function(){
-        like[i].classList.toggle('content__images-id-liked');
-    });
-};
-
